ce6cc4ae7a6dbcdde1a964a258bfb4da0c567ae9
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-23 23:12:31 +0100

    routes updated

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      localStorage.getItem('token') ? (
+      localStorage.getItem("token") ? (
         <Component {...props} />
       ) : (
-        <Redirect to='/' />
+        <Redirect to="/login" />
       )
     }
   />
