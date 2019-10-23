7b5567e627675e8bf9bbfbbf9b7286ed3437dd46
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-23 22:52:42 +0100

    Completed the styling of EditTodoForm

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AddTodoForm from "./AddTodoForm";
-import { Box, Flex, Stack } from "@chakra-ui/core";
+import { Box, Flex, Heading } from "@chakra-ui/core";
 import LeftNavigation, { SideNavLink } from "./LeftNavigation";
 import EditTodoForm from "./EditTodoForm";
 import TodoList from "./TodoList";
@@ -9,27 +9,25 @@ import Navigation from "./Navigation";
 import PrivateRoute from "./PrivateRoute";
 
 function Container(props) {
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState("");
   return (
     <Box>
-
-      <Navigation 
-        setSearchTerm={setSearchTerm}
-        searchTerm={searchTerm}
-      />
-      <Flex bg='gray.50' minHeight='100vh'>
-        <Box paddingTop='100px' as='aside' id='side-nav' flexBasis='20%'>
-
+      <Navigation setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+      <Flex bg="gray.50" minHeight="100vh">
+        <Box paddingTop="100px" as="aside" id="side-nav" flexBasis="20%">
           <LeftNavigation />
         </Box>
         <Box
-          py="100px"
+          py="50px"
           px="40px"
           bg="white"
           as="main"
           id="content-area"
           flexBasis="50%"
         >
+          <Heading size="lg" marginBottom="20px">
+            Tasks
+          </Heading>
           <AddTodoForm />
           <TodoList searchTerm={searchTerm} />
         </Box>
--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -9,87 +9,137 @@ import {
   Stack,
   Flex,
   FormErrorMessage,
-  Checkbox
+  Checkbox,
+  Text
 } from "@chakra-ui/core";
 import { withFormik, Field, Form } from "formik";
 import { connect } from "react-redux";
+import { FaCalendarAlt } from "react-icons/fa";
+
 import * as actionCreators from "../actionCreators";
 import * as yup from "yup";
 
 export function EditTodoForm(props) {
   //   debugger;
   return (
-    <Form>
-      <Field
-        name="task"
-        render={({ field, form }) => (
-          <FormControl
-            isInvalid={form.errors[field.name] && form.touched[field.name]}
+    <Box
+      marginX="20px"
+      py={3}
+      fontWeight="bold"
+      bg="white"
+      marginTop="30px"
+      padding="20px"
+      borderRadius="5px"
+      borderWidth="2px"
+    >
+      <Form>
+        <Text paddingY="20px">Edit Task</Text>
+        <Field
+          name="task"
+          render={({ field, form }) => (
+            <FormControl
+              isInvalid={form.errors[field.name] && form.touched[field.name]}
+            >
+              {/* <FormLabel htmlFor="task">First name</FormLabel> */}
+              <Input
+                {...field}
+                id="task"
+                placeholder="Enter task"
+                focusBorderColor="cyan.100"
+              />
+              <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        <Field
+          name="setDate"
+          render={({ field, form }) => (
+            <FormControl
+              isInvalid={form.errors[field.name] && form.touched[field.name]}
+            >
+              <Flex paddingY="20px">
+                <FormLabel htmlFor="set-date" marginTop="5px">
+                  Due{" "}
+                </FormLabel>
+                <Input
+                  {...field}
+                  type="date"
+                  id="set-date"
+                  marginLeft="20px"
+                  focusBorderColor="cyan.100"
+                  placeholder="Enter due date"
+                  maxWidth="180px"
+                />
+                <Box
+                  as={FaCalendarAlt}
+                  size="24px"
+                  marginTop="8px"
+                  color="gray.500"
+                  marginLeft="20px"
+                />
+              </Flex>
+              <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        <Field
+          name="completed"
+          render={({ field, form }) => (
+            <FormControl
+              isInvalid={form.errors[field.name] && form.touched[field.name]}
+            >
+              <Flex>
+                <FormLabel htmlFor="set-date">Completed?</FormLabel>
+                <Checkbox
+                  variantColor="cyan"
+                  id="completed"
+                  isChecked={field.value}
+                  {...field}
+                />
+                <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+              </Flex>
+            </FormControl>
+          )}
+        />
+        <Field
+          name="repeat"
+          render={({ field, form }) => (
+            <FormControl
+              isInvalid={form.errors[field.name] && form.touched[field.name]}
+            >
+              <Flex justify="space-between" align="center" paddingY="10px">
+                <FormLabel htmlFor="repeat">No Repeat</FormLabel>
+                <Select
+                  focusBorderColor="cyan.100"
+                  id="repeat"
+                  maxWidth="150px"
+                  placeholder="No Repeat"
+                  {...field}
+                >
+                  <option value="daily">Daily</option>
+                  <option value="weekly">Weekly</option>
+                  <option value="monthly">Monthly</option>
+                </Select>
+              </Flex>
+              <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        <Flex marginY="30px">
+          <Button type="submit" variantColor="cyan" color="white">
+            Save
+          </Button>
+          <Button
+            marginLeft="20px"
+            onClick={() => {
+              props.history.push("/todoapp");
+            }}
           >
-            {/* <FormLabel htmlFor="task">First name</FormLabel> */}
-            <Input {...field} id="task" placeholder="Enter task" />
-            <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      <Field
-        name="setDate"
-        render={({ field, form }) => (
-          <FormControl
-            isInvalid={form.errors[field.name] && form.touched[field.name]}
-          >
-            <FormLabel htmlFor="set-date">Due </FormLabel>
-            <Input
-              {...field}
-              type="date"
-              id="set-date"
-              placeholder="Enter due date"
-              maxWidth="180px"
-            />
-            <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      <Field
-        name="completed"
-        render={({ field, form }) => (
-          <FormControl
-            isInvalid={form.errors[field.name] && form.touched[field.name]}
-          >
-            <FormLabel htmlFor="set-date">Completed?</FormLabel>
-            <Checkbox id="completed" isChecked={field.value} {...field} />
-            <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      <Field
-        name="repeat"
-        render={({ field, form }) => (
-          <FormControl
-            isInvalid={form.errors[field.name] && form.touched[field.name]}
-          >
-            <FormLabel htmlFor="repeat">No Repeat</FormLabel>
-            <Select id="repeat" placeholder="No Repeat" {...field}>
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="monthly">Monthly</option>
-            </Select>
-            <FormErrorMessage>{form.errors[field.name]}</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-
-      <Button type="submit" paddingX="40px" variantColor="cyan" color="white">
-        Save
-      </Button>
-      <Button
-        onClick={() => {
-          props.history.push("/todoapp");
-        }}
-      >
-        Cancel
-      </Button>
-    </Form>
+            Cancel
+          </Button>
+        </Flex>
+      </Form>
+    </Box>
   );
 }
 
